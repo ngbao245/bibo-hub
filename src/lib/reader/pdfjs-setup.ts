60af2c7f50d3da01bf0ef0b051ec4a33b2215e83
baobibo <68727365+ngbao245@users.noreply.github.com>
2026-06-18 16:43:23 +0700

    Update pdfjs-setup.ts

--- src/lib/reader/pdfjs-setup.ts
+++ src/lib/reader/pdfjs-setup.ts
@@ -4,6 +4,12 @@ pdfjs.GlobalWorkerOptions.workerSrc =
   `${window.location.origin}/hubibo/pdf.worker.min.mjs`;
 
 console.log(
+  'window.location.origin',
+  window.location.origin
+);
+
+
+console.log(
   'WORKER:',
   pdfjs.GlobalWorkerOptions.workerSrc
 );
