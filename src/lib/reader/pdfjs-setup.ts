e47d3f19ce5455933d9ac6d03d80de4e9e49a0a9
baobibo <68727365+ngbao245@users.noreply.github.com>
2026-06-18 16:49:52 +0700

    Update pdfjs-setup.ts

--- src/lib/reader/pdfjs-setup.ts
+++ src/lib/reader/pdfjs-setup.ts
@@ -1,19 +1,6 @@
 import { pdfjs } from 'react-pdf';
 
-pdfjs.GlobalWorkerOptions.workerSrc =
-  `${window.location.origin}/hubibo/pdf.worker.min.mjs`;
-
-console.log(
-  'window.location.origin',
-  window.location.origin
-);
-
-console.log(
-  'pdfjs version',
-  pdfjs.version
-);
-
-console.log(
-  'WORKER:',
-  pdfjs.GlobalWorkerOptions.workerSrc
-);
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url
+).toString();
